d67780afea0b89a421196fc978b9dd250a8f3370
Christoffer Holmgren <christoffer.holmgren@gmail.com>
2015-12-02 22:38:46 +0100

    Simplified rendering of quiz, naming and more.
    - Quiz: Moved out the radiobuttons to a separate loop, changed some variable names and such to be more descriptive.
    - Reducer - Quiz: Simplified the code here, since we do not need the optionCount and option1-2-3 variables, instead we just use arrays.
    - Initialstate: Changed from variables to arrays, since they are much easier to handle dynamicly. Also removed some unused things on the quiz state object.

--- src/initialstate.js
+++ src/initialstate.js
@@ -1,44 +1,47 @@
 /*
-This is the initial state of the Redux Store. I store it in a separate file because I also use
-it in the reducers when we do the Reset action.
+ This is the initial state of the Redux Store. I store it in a separate file because I also use
+ it in the reducers when we do the Reset action.
 
-It returns a function instead of an object to make sure no one can change the initial state.
-*/
+ It returns a function instead of an object to make sure no one can change the initial state.
+ */
 
 var C = require("./constants");
 
-module.exports = function(){
-
-  //Question objects
-  var startQuestion = {question: "blabla?", answer: "bla", options: ["blabla","blablabla", "bla"], answerID: "A3"};
-  var question1 = { question: "Who is the president if the United States?", answer: "Barack Obama", options: ["Angela Merkel","Barack Obama", "David Cameron"], answerID: "A2"};
-  var question2 =  { question: "Do you like cookies?", answer: "Yes", options: ["Yes","No", "Dont know"], answerID: "A1"};
-
+module.exports = function () {
     return {
         doStuff: {
             currentValue: "",
-			currentSrcButton1: "https://secure.royalquiz.com/mq/images/lvvrcfqc.jpg",
-			currentSrcButton2: "https://secure.royalquiz.com/mq/images/lvvrcfqc.jpg",
-			currentSrcButton3: "https://secure.royalquiz.com/mq/images/lvvrcfqc.jpg",
-			currentSrcButton4: "https://secure.royalquiz.com/mq/images/lvvrcfqc.jpg"
-		},
-			quiz:{
-				questionValue: "",
-        questionArray: [startQuestion, question1, question2],
-				question: "",
-				checkedBox: "",
-				questionCount: 1,
-				optionCount: 0,
-				option1: "",
-				option2: "",
-				option3: "",
-				answerCount: 1,
-        points: 0,
-        multiplier: 1,
-        choice:"A3",
-        mute: "Mute sound",
-        data: null,
-        flipped: false
-			}
+            currentSrcButtons: ["https://secure.royalquiz.com/mq/images/lvvrcfqc.jpg",
+                "https://secure.royalquiz.com/mq/images/lvvrcfqc.jpg",
+                "https://secure.royalquiz.com/mq/images/lvvrcfqc.jpg",
+                "https://secure.royalquiz.com/mq/images/lvvrcfqc.jpg"],
+        },
+        quiz: {
+            questionValue: "",
+            questionArray: [{
+                question: "blabla?",
+                answer: "bla",
+                options: ["blabla", "blablabla", "bla"],
+                answerID: "A3"
+            }, {
+                question: "Who is the president if the United States?",
+                answer: "Barack Obama",
+                options: ["Angela Merkel", "Barack Obama", "David Cameron"],
+                answerID: "A2"
+            }, {
+                question: "Do you like cookies?",
+                answer: "Yes",
+                options: ["Yes", "No", "Dont know"],
+                answerID: "A1"
+            }],
+            currentQuestion: {options: []},
+            questionCount: 1,
+            points: 0,
+            multiplier: 1,
+            choice: "A3",
+            mute: "Mute sound",
+            data: null,
+            flipped: false
+        }
     }
 };
